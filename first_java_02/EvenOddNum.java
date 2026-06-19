b922c0cb861bff6d5691b22b8498e8dd32d9b1ba
samratbarar <samratbarar7707@gmail.com>
2026-06-19 14:29:29 +0530

    Done Even Odd num checker and Greet message printer

---
+++ first_java_02/EvenOddNum.java
@@ -0,0 +1,20 @@
+package first_java_02;
+
+import java.util.*;
+
+// Write a program to print whether a number is even or odd, also take input from the user.
+
+public class EvenOddNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        if (sc.nextInt() % 2 == 0) {
+            System.out.println("Given number is even number");            
+        } else {
+            System.out.println("Given number is Odd");
+        }
+
+        sc.close();
+    }
+}
---
+++ first_java_02/GreetByName.java
@@ -0,0 +1,14 @@
+package first_java_02;
+
+import java.util.*;
+
+// Take name as input and print a greeting message for that particular name.
+
+public class GreetByName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name ");
+        System.out.println("Hello, " + sc.nextLine() + ", Good morning");
+    }
+}
