7ac1d033353a7141d8f572c0d9409dc5728aeee4
samratbarar <samratbarar7707@gmail.com>
2026-06-20 21:49:10 +0530

    Distance between two points

---
+++ conditionals_loop_03/Medium_Ques/Ques_05.java
@@ -0,0 +1,18 @@
+package conditionals_loop_03.Medium_Ques;
+
+import java.util.*;
+
+// Calculate Distance Between Two Points
+
+public class Ques_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the x and y coordinates of both points (pair (x,y) of each point)");
+        double x1 = sc.nextDouble(), y1 = sc.nextDouble(), x2 = sc.nextDouble(), y2 = sc.nextDouble();
+
+        System.out.println("The distance between two points is " + ((double)(Math.sqrt(Math.pow((y2-y1), 2) + Math.pow((x2-x1), 2)))));
+
+        sc.close();
+    }
+}
