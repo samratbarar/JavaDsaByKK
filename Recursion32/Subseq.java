9c695cd853b9408b735afb324a182afb750a31f0
samratbarar <samratbarar7707@gmail.com>
2026-07-20 23:55:57 +0530

    Printing all subsequences of string

--- Recursion32/Subseq.java
+++ Recursion32/Subseq.java
@@ -2,21 +2,21 @@ package Recursion32;
 
 import java.util.*;
 
-public class Subsets {
-    public static void subsets(String str, String subStr) {
+public class Subseq {
+    public static void subseq(String str, String subStr) {
         if (str.isEmpty()) {
             System.out.println(subStr + " ");
             return;
         }
 
-        subsets(str.substring(1), subStr + str.charAt(0));
-        subsets(str.substring(1), subStr);
+        subseq(str.substring(1), subStr + str.charAt(0));
+        subseq(str.substring(1), subStr);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the string");
-        subsets(sc.nextLine(), "");
+        subseq(sc.nextLine(), "");
 
         sc.close();
     }
