0f89571007d3adf035f9c7ee3fbd3d09d86dd69f
samratbarar <samratbarar7707@gmail.com>
2026-07-23 01:18:02 +0530

    Count the paths to reach corner end

--- Recursion35/CountingMazePaths.java
+++ Recursion35/CountingMazePaths.java
@@ -3,26 +3,26 @@ package Recursion35;
 import java.util.*;
 
 public class CountingMazePaths {
-    static int countPaths(int rows, int cols, int row, int col) {
-        if (row == rows && col == cols) {
+    static int countPaths(int rows, int cols) {
+        if (rows == 1 && cols == 1) {
             return 1;
         }
 
-        if (row == rows) {
-            return countPaths(rows, cols, row, col + 1);
+        if (rows == 1) {
+            return countPaths(rows, cols - 1);
         }
 
-        if (col == cols) {
-            return countPaths(rows, cols, row + 1, col);           
+        if (cols == 1) {
+            return countPaths(rows - 1, cols);           
         }
 
-        return (countPaths(rows, cols, row + 1, col) + countPaths(rows, cols, row, col + 1));
+        return (countPaths(rows - 1, cols) + countPaths(rows, cols - 1));
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter Dimensions of maze");
-        int count = countPaths(sc.nextInt(), sc.nextInt(), 1, 1);
+        int count = countPaths(sc.nextInt(), sc.nextInt());
 
         System.out.println("The number of paths is " + count);
 
