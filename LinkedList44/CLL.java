cd7e4361ec55035a62dcb2dde590dd3df0d3f408
samratbarar <samratbarar7707@gmail.com>
2026-08-04 01:48:00 +0530

    Circular Linked List implementation

--- LinkedList44/CLL.java
+++ LinkedList44/CLL.java
@@ -1,7 +1,69 @@
 package LinkedList44;
 
-import java.util.*;
-
 public class CLL {
-    
+    private Node head;
+    private Node tail;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void insert(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+
+    public void display() {
+        Node temp = head;
+
+        if (head != null) {
+            do {
+                System.out.print(temp.value + " -> ");
+                temp = temp.next;
+            } while (temp != head);
+        }
+        System.out.print("HEAD");
+    }
+
+    public void delete(int value) {
+        Node temp = head;
+
+        if (temp == null) {
+            return;
+        }
+
+        if (temp.value == value) {
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do {
+            Node node = temp.next;
+            if (node.value == value) {
+                temp.next = node.next;
+                break;
+            }
+
+            temp = temp.next;
+        } while (temp != head);      
+    }
 }
